b4158516caf970c8819aba33d9b0d2687e8041f7
VasylievYurii <uraplanetianin@gmail.com>
2023-09-09 18:16:10 +0300

    Update ListTemplate.styled.js

--- src/components/ListTemplate/ListTemplate.styled.js
+++ src/components/ListTemplate/ListTemplate.styled.js
@@ -6,6 +6,10 @@ export const List = styled.ul`
   flex-wrap: wrap;
   padding-top: 10px;
   padding-bottom: 40px;
+  justify-content: center;
+  @media screen and (min-width: 860px) {
+    justify-content: start;
+  }
   @media screen and (min-width: 1440px) {
     padding-left: 30px;
     padding-right: 30px;
